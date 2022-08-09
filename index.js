66b98409ff4e2e19bcdba16d3f916df6eb7db75e
Kolonel1 <victor.munyao@student.moringaschool.com>
2022-08-09 23:38:49 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(x,y){return(x+=y);}
+function subtract(x,y){return(x-=y);}
+function multiply(x,y){return(x*=y);}
+function divide(x,y){return(x/=y);}
+//now to increment and decrement functions which i do not remember reading
+
+let number = 5;
+number ++;
+console.log(number);
+--number;
+console.log(number)
+
+number+=3;
+console.log(number)
+number-=4;
+console.log(number)
+
+
+// it seems number--an actual number is not possible 
+// now to type in an example of a code thwy have written
+
+number = 4
+function add4(){return (number +=5);}
+function divide3(){return (number /=3);}
+
+divide3();
+add4();
+console.log(number);
+let n=5
+function increment(n){return(++n);}
+function decrement(n){return(--n);}
+
+function makeInt(n){return(parseInt(n,10));}
+function preserveDecimal(n){return(parseFloat(n));}
+
